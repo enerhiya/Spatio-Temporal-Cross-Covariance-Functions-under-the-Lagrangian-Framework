55e953bd2e03691430e60a2fc2f31c6aa2837bd5
marysalvana <mary.salvana@obf.ateneo.edu>
2020-07-19 20:24:43 +0300

    added fast function to simulate multivariate normal using 25 cores in simulation.R

--- scripts/simulation.R
+++ scripts/simulation.R
@@ -5,17 +5,25 @@ root <- paste(directory, 'Spatio-Temporal-Cross-Covariance-Functions-under-the-L
 source(file = paste(root, "Functions/load_packages.R", sep = ''))
 source(file = paste(root, "Functions/cov_func.R", sep = ''))
 
-n <- 121
-N <- sqrt(n)
+N <- 30
 t <- 3
 grid_x <- seq(from = 0, to = 1, length.out = N)
 grid_y <- seq(from = 0, to = 1, length.out = N)
 sim_grid_locations <- expand.grid(grid_x, grid_y) %>% as.matrix()
 
 wind <- c(0.1001, 0.1001)
-theta <- c(1, 1, 0.23, 0.5, 1)
+theta <- c(1, 1, 0.23, 0.5, 1, 0.5)
 
 cov1 <- matern_cov(theta, wind, max_time_lag = t - 1, LOCS = sim_grid_locations)
 
+# call function to plot the covariance
+
+jpeg(file = paste(root, 'Figures/heatmap_covariance.jpg', sep = ''), width = 800, height = 800)
+
+image.plot(cov1)
+
+dev.off()
+
 set.seed(1234)
-r1 <- mvrnorm(1, mu = rep(0, ncol(cov1)), Sigma = cov1)
+r1 <- rmvn(1, rep(0, ncol(cov1)), cov1, ncores = 25)
+
