5bfc6e47b7f2bd169235c29fc192e749088fbd89
marysalvana <mary.salvana@obf.ateneo.edu>
2021-02-04 11:55:36 +0300

    updated codes for wind data plots

--- scripts/1-3_wind_plots.R
+++ scripts/1-3_wind_plots.R
@@ -10,9 +10,9 @@ saudi<- map("world", "Saudi", fill = TRUE)
 IDs <- sapply(strsplit(saudi$names, ":"), function(x) x[1])
 saudi <- map2SpatialPolygons(saudi, IDs=IDs, proj4string=CRS("+proj=longlat +datum=WGS84"))
 
-WIND_MAT <- matrix(, ncol = 4, nrow = 40)
+WIND_ARRAY <- array(, dim = c(550 * 5, 4, 20))
 
-for (yr in 1980:2019){
+for (yr in 2000:2019){
 
 	wind_U_component <- wind_V_component <- list()
 
@@ -112,15 +112,94 @@ for (yr in 1980:2019){
 		wind_V_component[[VAR]] <- data_array2
 
 	}
-	WIND_MAT[yr - 1979, 1] <- mean(wind_U_component[[1]][140:144, ])
-	WIND_MAT[yr - 1979, 2] <- mean(wind_V_component[[1]][140:144, ])
-	WIND_MAT[yr - 1979, 3] <- mean(wind_U_component[[2]][140:144, ])
-	WIND_MAT[yr - 1979, 4] <- mean(wind_V_component[[2]][140:144, ])
+
+	WIND_ARRAY[, 1, yr - 1999] <- c(wind_U_component[[1]][140:144, ])
+	WIND_ARRAY[, 2, yr - 1999] <- c(wind_V_component[[1]][140:144, ])
+	WIND_ARRAY[, 3, yr - 1999] <- c(wind_U_component[[2]][140:144, ])
+	WIND_ARRAY[, 4, yr - 1999] <- c(wind_V_component[[2]][140:144, ])
 }
 
+test <- round(cbind(c(WIND_ARRAY[, 2, ]), c(WIND_ARRAY[, 1, ])), 2)
+
+#pdf(file = paste('/home/salvanmo/Desktop/thesis/thesis-defense/Figures/wind_data.pdf', sep = ''), width = 15, height = 15)
+jpeg(file = paste('/home/salvanmo/Desktop/thesis/thesis-defense/Figures/wind_data.jpg', sep = ''), width = 1500, height = 1500)
+
+split.screen( rbind(c(0.06, 0.98, 0.05, 0.98), c(0.99,0.99,0.05,0.95)))
+split.screen( figs = c( 3, 3 ), screen = 1 )
+
+screen(3)
+par(pty = 's')
+par(mai=c(0.2,0.2,0.2,0.2))
+
+plot(unique(round(cbind(c(WIND_ARRAY[, 2, ]), c(WIND_ARRAY[, 1, ])), 0)), xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 0.5, col = "#808080", ylim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), xlim = c(-max(WIND_ARRAY), max(WIND_ARRAY)))
+abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
+mtext(bquote(paste(v[y], " (880 hPa) ")), side = 3, line = 0, adj = 0.5, cex = 2)
+mtext(bquote(paste(v[x], " (880 hPa) ")) , side = 2, line = 3, adj = 0.5, cex = 2)
+#ellipse(mu = c(0.13178367, 0.00721022), sigma = matrix(c(27.160213, 4.917272, 4.917272, 5.554407), 2, 2), alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 4) 
+ellipse(mu = c(mu1_ms[2], mu1_ms[1]), sigma = wind_var1[c(2,1), c(2,1)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu1_ms[2], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
+
+screen(11)
+par(pty = 's')
+par(mai=c(0.2,0.2,0.2,0.2))
+
+plot(unique(round(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 3, ])), 0)), xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 0.5, col = "#808080", ylim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), xlim = c(-max(WIND_ARRAY), max(WIND_ARRAY)))
+abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
+mtext(bquote(paste(v[x], " (925 hPa) ")) , side = 2, line = 3, adj = 0.5, cex = 2)
+#ellipse(mu = c(0.139535500, -0.006487013), sigma = matrix(c(12.8859287, -0.2185663, -0.2185663, 4.4855505), 2, 2), alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 4) 
+#ellipse(mu = colMeans(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 3, ]))), sigma = var(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 3, ]))), alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 4) 
+ellipse(mu = c(mu2_ms[2], mu2_ms[1]), sigma = wind_var2[c(2,1), c(2,1)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[2], mu2_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
+
+screen(4)
+par(pty = 's')
+par(mai=c(0.2,0.2,0.2,0.2))
+
+plot(unique(round(cbind(c(WIND_ARRAY[, 3, ]), c(WIND_ARRAY[, 1, ])), 0)), xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 0.5, col = "#808080", ylim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), xlim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), yaxt = 'n', xaxt = 'n')
+abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
+mtext(bquote(paste(v[x], " (925 hPa) ")), side = 3, line = 0, adj = 0.5, cex = 2)
+#ellipse(mu = colMeans(cbind(c(WIND_ARRAY[, 3, ]), c(WIND_ARRAY[, 1, ]))), sigma = var(cbind(c(WIND_ARRAY[, 3, ]), c(WIND_ARRAY[, 1, ]))), alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 4) 
+#ellipse(mu = c(mu2_ms[1], mu1_ms[1]), sigma = wind_var_ms2[c(3, 1), c(3, 1)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[1], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
+  
+screen(8)
+par(pty = 's')
+par(mai=c(0.2,0.2,0.2,0.2))
+
+plot(unique(round(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 2, ])), 0)), xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 0.5, col = "#808080", ylim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), xlim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), yaxt = 'n', xaxt = 'n')
+abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
+#ellipse(mu = colMeans(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 2, ]))), sigma = var(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 2, ]))), alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 4) 
+#ellipse(mu = c(mu2_ms[2], mu1_ms[2]), sigma = wind_var_ms2[c(4, 2), c(4, 2)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[2], mu1_ms[2]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
+
+screen(5)
+par(pty = 's')
+par(mai=c(0.2,0.2,0.2,0.2))
+
+plot(unique(round(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 1, ])), 0)), xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 0.5, col = "#808080", ylim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), xlim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), yaxt = 'n', xaxt = 'n')
+abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
+mtext(bquote(paste(v[y], " (925 hPa) ")), side = 3, line = 0, adj = 0.5, cex = 2)
+#ellipse(mu = colMeans(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 1, ]))), sigma = var(cbind(c(WIND_ARRAY[, 4, ]), c(WIND_ARRAY[, 1, ]))), alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 4) 
+#ellipse(mu = c(mu2_ms[2], mu1_ms[1]), sigma = wind_var_ms2[c(4, 1), c(4, 1)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[2], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
+
+screen(7)
+par(pty = 's')
+par(mai=c(0.2,0.2,0.2,0.2))
+
+plot(unique(round(cbind(c(WIND_ARRAY[, 3, ]), c(WIND_ARRAY[, 2, ])), 0)), xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 0.5, col = "#808080", ylim = c(-max(WIND_ARRAY), max(WIND_ARRAY)), xlim = c(-max(WIND_ARRAY), max(WIND_ARRAY)))
+abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
+mtext(bquote(paste(v[y], " (880 hPa) ")) , side = 2, line = 3, adj = 0.5, cex = 2)
+#ellipse(mu = colMeans(cbind(c(WIND_ARRAY[, 3, ]), c(WIND_ARRAY[, 2, ]))), sigma = var(cbind(c(WIND_ARRAY[, 3, ]), c(WIND_ARRAY[, 2, ]))), alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 4) 
+#ellipse(mu = c(mu2_ms[1], mu1_ms[2]), sigma = wind_var_ms2[c(3, 2), c(3, 2)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[1], mu1_ms[2]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
+
+close.screen( all=TRUE)
+dev.off()
 
 
-pdf(file = paste('/home/salvanmo/Desktop/thesis/thesis-defense/Figures/wind_data.pdf', sep = ''), width = 15, height = 15)
+
+pdf(file = paste('/home/salvanmo/Desktop/thesis/thesis-defense/Figures/wind_data_OLDOLD.pdf', sep = ''), width = 15, height = 15)
 
 split.screen( rbind(c(0.06, 0.98, 0.05, 0.98), c(0.99,0.99,0.05,0.95)))
 split.screen( figs = c( 3, 3 ), screen = 1 )
@@ -133,8 +212,8 @@ plot(WIND_MAT[, c(2, 1)], xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 
 abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
 mtext(bquote(paste(v[y], " (880 hPa) ")), side = 3, line = 0, adj = 0.5, cex = 2)
 mtext(bquote(paste(v[x], " (880 hPa) ")) , side = 2, line = 3, adj = 0.5, cex = 2)
-ellipse(mu = c(mu1_ms[2], mu1_ms[1]), sigma = wind_var_ms2[c(2,1), c(2,1)], alpha = .05, npoints = 250, col = "#e54457", lwd = 2) 
-points(matrix(c(mu1_ms[2], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#e54457")
+ellipse(mu = c(mu1_ms[2], mu1_ms[1]), sigma = wind_var_ms2[c(2,1), c(2,1)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu1_ms[2], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
 
 screen(11)
 par(pty = 's')
@@ -143,8 +222,8 @@ par(mai=c(0.2,0.2,0.2,0.2))
 plot(WIND_MAT[, c(4, 3)], xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 1, col = "#808080", ylim = c(-max(WIND_MAT), max(WIND_MAT)), xlim = c(-max(WIND_MAT), max(WIND_MAT)))
 abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
 mtext(bquote(paste(v[x], " (925 hPa) ")) , side = 2, line = 3, adj = 0.5, cex = 2)
-ellipse(mu = c(mu2_ms[2], mu2_ms[1]), sigma = wind_var_ms2[c(4, 3), c(4, 3)], alpha = .05, npoints = 250, col = "#e54457", lwd = 2) 
-points(matrix(c(mu2_ms[2], mu2_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#e54457")
+ellipse(mu = c(mu2_ms[2], mu2_ms[1]), sigma = wind_var_ms2[c(4, 3), c(4, 3)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[2], mu2_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
 
 screen(4)
 par(pty = 's')
@@ -153,8 +232,8 @@ par(mai=c(0.2,0.2,0.2,0.2))
 plot(WIND_MAT[, c(3, 1)], xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 1, col = "#808080", ylim = c(-max(WIND_MAT), max(WIND_MAT)), xlim = c(-max(WIND_MAT), max(WIND_MAT)), yaxt = 'n', xaxt = 'n')
 abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
 mtext(bquote(paste(v[x], " (925 hPa) ")), side = 3, line = 0, adj = 0.5, cex = 2)
-ellipse(mu = c(mu2_ms[1], mu1_ms[1]), sigma = wind_var_ms2[c(3, 1), c(3, 1)], alpha = .05, npoints = 250, col = "#e54457", lwd = 2) 
-points(matrix(c(mu2_ms[1], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#e54457")
+ellipse(mu = c(mu2_ms[1], mu1_ms[1]), sigma = wind_var_ms2[c(3, 1), c(3, 1)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[1], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
   
 screen(8)
 par(pty = 's')
@@ -162,8 +241,8 @@ par(mai=c(0.2,0.2,0.2,0.2))
 
 plot(WIND_MAT[, c(4, 2)], xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 1, col = "#808080", ylim = c(-max(WIND_MAT), max(WIND_MAT)), xlim = c(-max(WIND_MAT), max(WIND_MAT)), yaxt = 'n', xaxt = 'n')
 abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
-ellipse(mu = c(mu2_ms[2], mu1_ms[2]), sigma = wind_var_ms2[c(4, 2), c(4, 2)], alpha = .05, npoints = 250, col = "#e54457", lwd = 2) 
-points(matrix(c(mu2_ms[2], mu1_ms[2]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#e54457")
+ellipse(mu = c(mu2_ms[2], mu1_ms[2]), sigma = wind_var_ms2[c(4, 2), c(4, 2)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[2], mu1_ms[2]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
 
 screen(5)
 par(pty = 's')
@@ -172,8 +251,8 @@ par(mai=c(0.2,0.2,0.2,0.2))
 plot(WIND_MAT[, c(4, 1)], xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 1, col = "#808080", ylim = c(-max(WIND_MAT), max(WIND_MAT)), xlim = c(-max(WIND_MAT), max(WIND_MAT)), yaxt = 'n', xaxt = 'n')
 abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
 mtext(bquote(paste(v[y], " (925 hPa) ")), side = 3, line = 0, adj = 0.5, cex = 2)
-ellipse(mu = c(mu2_ms[2], mu1_ms[1]), sigma = wind_var_ms2[c(4, 1), c(4, 1)], alpha = .05, npoints = 250, col = "#e54457", lwd = 2) 
-points(matrix(c(mu2_ms[2], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#e54457")
+ellipse(mu = c(mu2_ms[2], mu1_ms[1]), sigma = wind_var_ms2[c(4, 1), c(4, 1)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[2], mu1_ms[1]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
 
 screen(7)
 par(pty = 's')
@@ -182,8 +261,8 @@ par(mai=c(0.2,0.2,0.2,0.2))
 plot(WIND_MAT[, c(3, 2)], xlab = "", ylab = "", cex.axis = 1.5, pch = 20, cex = 1, col = "#808080", ylim = c(-max(WIND_MAT), max(WIND_MAT)), xlim = c(-max(WIND_MAT), max(WIND_MAT)))
 abline(h = 0, v = 0, col = 1, lwd = 2, lty = 2)
 mtext(bquote(paste(v[y], " (880 hPa) ")) , side = 2, line = 3, adj = 0.5, cex = 2)
-ellipse(mu = c(mu2_ms[1], mu1_ms[2]), sigma = wind_var_ms2[c(3, 2), c(3, 2)], alpha = .05, npoints = 250, col = "#e54457", lwd = 2) 
-points(matrix(c(mu2_ms[1], mu1_ms[2]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#e54457")
+ellipse(mu = c(mu2_ms[1], mu1_ms[2]), sigma = wind_var_ms2[c(3, 2), c(3, 2)], alpha = .05, npoints = 250, col = "#3fb1e2", lwd = 2) 
+points(matrix(c(mu2_ms[1], mu1_ms[2]), ncol = 2), pch = 8, lwd = 4, cex = 2, col = "#3fb1e2")
 
 close.screen( all=TRUE)
 dev.off()
@@ -273,6 +352,9 @@ mu2 <- c(0.06025701, 0.07327084)
 mu1 <- c(0.04082589, 0.06223707)
 mu2 <- c(-0.02917227, 0.04565329)
 
+mu1 <- c(-0.073211462, -0.005373963)
+mu2 <- c(0.02514516, 0.01563370)
+
 mu1_ms <- (mu1 * 100 / 3) * (0.2778)
 mu2_ms <- (mu2 * 100 / 3) * (0.2778) 
 
@@ -282,8 +364,19 @@ wind_var_ms2 <- (wind_var * 10000 / 3^2) * 0.07716049
 
 wind_var_ms2 <- matrix(c(5.477262,  2.517276, 5.274911, 2.486674, 2.517276, 10.220525, 2.186165, 10.11981, 5.274911, 2.186165, 5.200563,  2.164414, 2.486674, 10.11981, 2.164414, 10.196487), 4, 4)
 
+p <- read.table(paste(root, 'Results/multivariate_stationary_real_M1_A_FULL', sep = ''), header = FALSE, sep = " ") %>% as.matrix()
 
+mu1 <- p[9:10]
+mu2 <- p[11:12]
 
+mu1_ms <- (mu1 * 100 / 3) * (0.2778)
+mu2_ms <- (mu2 * 100 / 3) * (0.2778) 
+
+wind_var_chol <- matrix(c(p[13], p[14], 0, p[15]), ncol = 2, byrow = T)
+wind_var1 <- t(wind_var_chol) %*% wind_var_chol
+
+wind_var_chol <- matrix(c(p[16], p[17], 0, p[18]), ncol = 2, byrow = T)
+wind_var2 <- t(wind_var_chol) %*% wind_var_chol
 
 
 
